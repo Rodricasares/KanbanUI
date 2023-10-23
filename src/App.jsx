f841e40d39e9d60be7daa7d722ad5e0badd35b65
Rodrigo <rodrigocantudo@gmail.com>
2023-10-23 09:23:58 +0200

    flexbox,object-fit/app.css & kanban.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-
-import './App.css'
-import { Kanban } from './assets/components/kanban/Kanban'
+import "./App.css";
+import { Kanban } from "./assets/components/kanban/Kanban";
 //import { Card } from './assets/components/Card'
 
-
-
 function App() {
-
   return (
-    <div style={{padding: '50px'}}>
-  <h1 style={{marginBottom:'20px'}}>Kanban UI</h1>
-<Kanban />
-  </div>
-  )
+    <div className="flex-container">
+      {" "}
+
+        <h1>Kanban UI</h1>
+        <Kanban />
+    </div>
+  );
 }
 
-export default App
+export default App;
